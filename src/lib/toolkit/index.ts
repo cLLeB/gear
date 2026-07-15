7490040fd472ca832371939777797169aaee2590
cLLeB <168653764+cLLeB@users.noreply.github.com>
2026-07-15 16:38:49 +0000

    feat(toolkit): unicode-safe slugify for branches and filenames

--- src/lib/toolkit/index.ts
+++ src/lib/toolkit/index.ts
@@ -3,9 +3,15 @@
 // and exposable through the command palette. Each export is a self-contained
 // feature; add new ones alphabetically and keep them dependency-free.
 
+export * from "./dedent";
+export * from "./escapeRegExp";
 export * from "./formatBytes";
+export * from "./formatNumberCompact";
 export * from "./fuzzyScore";
 export * from "./humanizeDuration";
+export * from "./ordinal";
+export * from "./pluralize";
 export * from "./semver";
+export * from "./slugify";
 export * from "./stripAnsi";
 export * from "./truncateMiddle";
